85b27c0e99264a308bd6d9e11045d818c556ecc3
hbadejo <badejohakeem95@gmail.com>
2022-10-19 04:04:30 +0100

    Letter in string repetition match token #1

---
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+
+##Requirements:
+
+#Find the regular expression that will match the string repetition 
+#Using the project instructions, create a Ruby script that accepts one argument and... 
+#pass it to a regular expression matching method
+
+puts ARGV[0].match(/hb?tn/)
