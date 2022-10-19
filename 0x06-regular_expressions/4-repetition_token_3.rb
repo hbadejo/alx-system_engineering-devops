e3f25e12eadffcb316c3e9ba44f69d82c9355076
hbadejo <badejohakeem95@gmail.com>
2022-10-19 04:08:09 +0100

    Repetition for token 2 and 3

---
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+
+##Requirements:
+
+#Find the regular expression that will match the string repetition 
+#Using the project instructions, create a Ruby script that accepts one argument and... 
+#pass it to a regular expression matching method
+#Your regex should not contain square brackets
+
+puts ARGV[0].match(/hbt*n/)
