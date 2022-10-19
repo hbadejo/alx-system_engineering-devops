ee8ceb6e3286acb9dcf1a0e055a9f5d36502f883
hbadejo <badejohakeem95@gmail.com>
2022-10-19 04:19:37 +0100

    Ten digit match

---
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+
+#This task is brought to you by a professional advisor Neha Jain, Senior Software Engineer at LinkedIn.
+
+##Requirement:
+
+#The regular expression must match a 10 digit phone number
+
+puts ARGV[0].match(/\d{10}/)
