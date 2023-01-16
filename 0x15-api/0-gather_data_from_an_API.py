5cf806d836a1a76c971c01be35b300d008b77ed8
hbadejo <badejohakeem95@gmail.com>
2023-01-16 22:57:18 +0100

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,6 +4,9 @@ import requests
 import sys
 
 if __name__ == "__main__":
+    """
+    Variable for links
+    """
     url = "https://jsonplaceholder.typicode.com/"
     emp = requests.get(url + "users/{}".format(sys.argv[1])).json()
     todo = requests.get(url + "todos", uid={"userId": sys.argv[1]}).json()
