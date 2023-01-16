0ec0c320031a77e2a7e329ac1cf21e3e90b60188
hbadejo <badejohakeem95@gmail.com>
2023-01-16 22:44:52 +0100

    API project

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Returns to-do list information for a given employee ID."""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    emp = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todo = requests.get(url + "todos", uid = {"userId": sys.argv[1]}).json()
+
+    completed = [data.get("title") for data in todo if data.get("comleted") is True]
+    print(f"Employee {emp.get(name)} is done with tasks({len(complete)}/{len(todos)})")
+    [print(f"\t {data}" for data in completed)
+
