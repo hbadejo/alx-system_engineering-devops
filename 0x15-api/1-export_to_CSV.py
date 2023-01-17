0406297f9649fbf3722856902670ca04c86f791b
hbadejo <badejohakeem95@gmail.com>
2023-01-17 10:10:20 +0100

    Export to CSV format_docstring

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Export to-do list information for a given employee to a CSV file."""
+"""Exports to-do list information for a given employee ID to CSV format."""
 
 import csv
 import requests
