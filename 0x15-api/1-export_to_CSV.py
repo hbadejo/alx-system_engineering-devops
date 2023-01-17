4f74940d6de07dc819cb1c4f63f5edd17aafe0be
hbadejo <badejohakeem95@gmail.com>
2023-01-17 09:51:57 +0100

    Export to CSV

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Export to-do list information for a given employee to a CSV file."""
+import csv
+import requests
+import sys
+
+
+def ex_emp_todo_csv():
+    """
+    Export to-do to CSV file
+
+    Requirements:
+        Records all tasks that are owned by this employee
+    Format must be: "USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
+    File name must be: USER_ID.csv
+    """
+
+
+    url = "https://jsonplaceholder.typicode.com/"
+    emp = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todo = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    user_id = sys.argv[1]
+
+
+    with open(f"{user_id}.csv", "w", newline="") as csvfile:
+        pen = csv.writer(csvfile)
+        [pen.writerow(
+            [user_id, emp.get("username"), data.get("complete"), data.get("title")]
+            ) for data in todo]
+
+
+if __name__ == "__main__":
+    ex_emp_todo_csv()
