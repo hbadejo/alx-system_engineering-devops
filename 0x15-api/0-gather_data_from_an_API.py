ee2a49fc58fa91e43aaac7ce231c7f29261fb2b2
hbadejo <badejohakeem95@gmail.com>
2023-01-17 08:50:42 +0100

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,14 +1,31 @@
-#!/usr/bin/python3
-"""Returns to-do list information for a given employee ID."""
+#! /usr/bin/env python
+# Returns to-do list information for a given employee ID.
 import requests
 import sys
 
-if __name__ == "__main__":
-    # Variable for links
+
+def emp_todo():
+    """
+    Python script that, using this REST API, 
+    for a given employee ID, 
+    returns information about his/her TODO list progress.
+
+    Requirements:
+        You must use urllib or requests module
+        The script must accept an integer as a parameter, 
+        which is the employee ID
+    """
     url = "https://jsonplaceholder.typicode.com/"
     emp = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todo = requests.get(url + "todos", uid={"userId": sys.argv[1]}).json()
+    todo = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    completed = [data.get("title") for data in todo if data.get("comleted") is True]
-    print(f"Employee {emp.get("name")} is done with tasks({len(completed)}/{len(todos)})")
-    [print(f"\t {data}" for data in completed)]
+    completed = [data.get("title")
+                 for data in todo if data.get("completed") is True]
+    print("Employee {} is done with tasks({}/{})".format(emp.get("name"),
+          len(completed), len(todo)))
+    [print("\t {}".format(com_title)) for com_title in completed]
+
+
+if __name__ == "__main__":
+    emp_todo()
+
